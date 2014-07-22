6d23ac7a19e8f3277f76db576083fac0763aa6f0
qingchengnus <qingchengnus@gmail.com>
2014-07-22 09:08:42 +0800

    changed shift encryption

--- bypasser/simpleshift.go
+++ bypasser/simpleshift.go
@@ -11,13 +11,15 @@ func NewShiftCipher(key byte) shiftCipher {
 func (c shiftCipher) Encrypt(dst, src []byte) {
 	srcLen := len(src)
 	for i := 0; i < srcLen; i++ {
-		dst[i] = byte((int(src[i]) + int(c.key)) % 256)
+		//dst[i] = byte((int(src[i]) + int(c.key)) % 256)
+		dst[i] = 0xff - src[i]
 	}
 }
 
 func (c shiftCipher) Decrypt(dst, src []byte) {
 	srcLen := len(src)
 	for i := 0; i < srcLen; i++ {
-		dst[i] = byte((int(src[i]) - int(c.key)) % 256)
+		//dst[i] = byte((int(src[i]) - int(c.key)) % 256)
+		dst[i] = 0xff - src[i]
 	}
 }
